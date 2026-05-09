12ee800ef772d7c23ce7f73b15e8b3632a3df01b
khslook79-pixel <khslook79@gmail.com>
2026-05-09 16:21:09 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const port = 4173;
+const host = "127.0.0.1";
+const mimeTypes = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+};
+
+const server = http.createServer((request, response) => {
+  const url = new URL(request.url, `http://${host}:${port}`);
+  const requestedPath = url.pathname === "/" ? "/index.html" : url.pathname;
+  const filePath = path.normalize(path.join(root, requestedPath));
+
+  if (!filePath.startsWith(root)) {
+    response.writeHead(403);
+    response.end("Forbidden");
+    return;
+  }
+
+  fs.readFile(filePath, (error, contents) => {
+    if (error) {
+      response.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      response.end("Not found");
+      return;
+    }
+
+    response.writeHead(200, {
+      "Content-Type": mimeTypes[path.extname(filePath)] || "application/octet-stream",
+    });
+    response.end(contents);
+  });
+});
+
+server.listen(port, host);
